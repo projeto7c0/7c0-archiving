1d503498cad649996bb9bfaf866640dcb649f7cf
Lucas Lago <lago@nic.br>
2020-07-05 16:02:59 -0300

    para arquivador remoto

--- arquivar_tweets.py
+++ arquivar_tweets.py
@@ -3,15 +3,14 @@ import archiveis
 import time
 
 def arquivar_tweets():
-    # print("Arquivando tweets...")
     lista_ids = database.recupera_ids_sem_arquivo()
     for par in lista_ids:
         url = "https://twitter.com/" + str(par[1]) + "/status/" + str(par[0])
-        print(url)
         try:
             url_arquivo = archiveis.capture(url)
-            print(url_arquivo)
+            # print(url_arquivo)
             database.adiciona_arquivo(par[0], url_arquivo)
         except Exception as E:
             print(E)
             print("Problema no arquivador principal")
+            exit()
--- database.py
+++ database.py
@@ -12,20 +12,6 @@ def recupera_ids_sem_arquivo():
     db.close()
     return value
 
-def recupera_100_ids():
-    sql = "select idTweets, handle from mimic_tweets order by idTweets DESC limit 200"
-    db = database_auth.conecta_banco()
-    cursor = db.cursor()
-    try:
-        cursor.execute(sql)
-        value = cursor.fetchall()
-    except Exception as E:
-        print(E)
-    db.close()
-    return value
-
-
-
 def adiciona_arquivo(id, url_arquivo):
     sql = "update mimic_tweets set archive_url =\""+url_arquivo+"\" where idTweets = \"" + str(id) + "\";"
     db = database_auth.conecta_banco()
