d32f8c26b50f0be13effad49bd46cb3906ef6009
Lucas Lago <lago@nic.br>
2020-09-27 15:22:52 -0300

    alterações eleições

--- arquivar_tweets.py
+++ arquivar_tweets.py
@@ -8,8 +8,21 @@ def arquivar_tweets():
         url = "https://twitter.com/" + str(par[1]) + "/status/" + str(par[0])
         try:
             url_arquivo = archiveis.capture(url)
-            # print(url_arquivo)
+            print(url_arquivo)
             database.adiciona_arquivo(par[0], url_arquivo)
+        except Exception as E:
+            print(E)
+            print("Problema no arquivador principal")
+            exit()
+
+def arquivar_tweets_election():
+    lista_ids = database.recupera_ids_sem_arquivo_election()
+    for par in lista_ids:
+        url = "https://twitter.com/" + str(par[1]) + "/status/" + str(par[0])
+        try:
+            url_arquivo = archiveis.capture(url)
+            print(url_arquivo)
+            database.adiciona_arquivo_election(par[0], url_arquivo)
         except Exception as E:
             print(E)
             print("Problema no arquivador principal")
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import database_auth
 
 def recupera_ids_sem_arquivo():
-    sql = "select idTweets, handle from mimic_tweets where archive_url is null order by idTweets DESC limit 1"
+    sql = "select idTweets, handle from mimic_tweets where archive_url is null order by idTweets ASC limit 5"
     db = database_auth.conecta_banco()
     cursor = db.cursor()
     try:
@@ -14,6 +14,29 @@ def recupera_ids_sem_arquivo():
 
 def adiciona_arquivo(id, url_arquivo):
     sql = "update mimic_tweets set archive_url =\""+url_arquivo+"\" where idTweets = \"" + str(id) + "\";"
+    db = database_auth.conecta_banco()
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+    except Exception as E:
+        print(E)
+    db.commit()
+    db.close()
+
+def recupera_ids_sem_arquivo_election():
+    sql = "select idTweets, handle from election_tweets_2020 where archive_url is null order by idTweets ASC limit 5"
+    db = database_auth.conecta_banco()
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        value = cursor.fetchall()
+    except Exception as E:
+        print(E)
+    db.close()
+    return value
+
+def adiciona_arquivo_election(id, url_arquivo):
+    sql = "update election_tweets_2020 set archive_url =\""+url_arquivo+"\" where idTweets = \"" + str(id) + "\";"
     db = database_auth.conecta_banco()
     cursor = db.cursor()
     try:
